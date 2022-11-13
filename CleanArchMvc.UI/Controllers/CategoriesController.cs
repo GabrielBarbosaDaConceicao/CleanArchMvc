c23245503a4dd1bcd38c23d3858e350ff058114b
Gabriel Conceicao <gabriel.bc.dev@hotmail.com>
2022-11-13 13:28:29 -0300

    Add controller Detais for Category

--- CleanArchMvc.UI/Controllers/CategoriesController.cs
+++ CleanArchMvc.UI/Controllers/CategoriesController.cs
@@ -93,5 +93,18 @@ namespace CleanArchMvc.WebUI.Controllers
             await _categoryService.Remove(id);
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var categoryDto = await _categoryService.GetById(id);
+
+            if (categoryDto == null)
+                return NotFound();
+            return View(categoryDto);
+        }
     }
 }

4c29964e54813b5ebef9b17b3f3cabee6633f0ec
Gabriel Conceicao <gabriel.bc.dev@hotmail.com>
2022-11-15 15:26:22 -0300

    Implementando seguranca usando o Identity

---
+++ CleanArchMvc.Domain/Account/IAuthenticate.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArchMvc.Domain.Account
+{
+    public interface IAuthenticate
+    {
+        Task<bool> Authenticate(string email, string password);
+
+        Task<bool> RegisterUser(string email, string password);
+
+        Task Logout();
+    }
+}

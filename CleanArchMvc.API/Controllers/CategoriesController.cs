66d64bd2446fd18348e1c9f9048f4ae46c18899b
Gabriel Conceicao <gabriel.bc.dev@hotmail.com>
2022-11-20 11:12:56 -0300

    Adicionado API ao projeto / controller Category /endpoints Get / GetId

---
+++ CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -0,0 +1,44 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var categories = await _categoryService.GetCategories();
+            if (categories == null)
+            {
+                return NotFound("Categories not found.");
+            }
+
+            return Ok(categories);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<CategoryDTO>> Get(int? id)
+        {
+            var category = await _categoryService.GetById(id);
+            if (category == null)
+            {
+                return NotFound("Category not found.");
+            }
+            return Ok(category);
+        }
+
+    }
+}

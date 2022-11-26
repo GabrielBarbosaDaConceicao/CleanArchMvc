9e6fe89cd97889e4326c4e52c2a293013112512d
Gabriel Conceicao <gabriel.bc.dev@hotmail.com>
2022-11-26 11:18:00 -0300

    Adicionado ControllerProducts / Endpoints Get/GetId/Post/Put/Delete

---
+++ CleanArchMvc.API/Controllers/ProductsController.cs
@@ -0,0 +1,79 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Products.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> Get()
+        {
+            var products = await _productService.GetProducts();
+
+            if (products == null)
+                return NotFound("Products not found");
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id:int}", Name = "GetProduct")]
+        public async Task<ActionResult<ProductDTO>> Get(int? id)
+        {
+            var productDto = await _productService.GetById(id);
+
+            if (productDto == null)
+                return NotFound("Product not found");
+
+            return Ok(productDto);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] ProductDTO productDto)
+        {
+            if (productDto == null)
+                return BadRequest("Data invalid");
+
+            await _productService.Add(productDto);
+
+            return new CreatedAtRouteResult("GetProduct",
+                new { id = productDto.Id }, productDto);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int? id, [FromBody] ProductDTO productDto)
+        {
+            if (id != productDto.Id)
+                return BadRequest("Data invalid");
+
+            if (productDto == null)
+                return BadRequest("Data invalid");
+
+            await _productService.Update(productDto);
+
+            return Ok(productDto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<ProductDTO>> Delete(int? id)
+        {
+            var productDto =  await _productService.GetById(id);
+
+            if (id == null) 
+                return NotFound("Product not found");
+
+            await _productService.Remove(id);
+
+            return Ok(productDto);
+        }
+    }
+}
--- CleanArchMvc.Application/DTOs/ProductDTO.cs
+++ CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace CleanArchMvc.Application.DTOs
 {
@@ -37,6 +38,7 @@ namespace CleanArchMvc.Application.DTOs
         [DisplayName("Product Image")]
         public string Image { get; set; }
 
+        [JsonIgnore]
         public Category Category { get; set; }
 
         [DisplayName("Categories")]

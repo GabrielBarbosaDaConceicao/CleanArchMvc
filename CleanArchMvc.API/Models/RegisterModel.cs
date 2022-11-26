42c8f67d283f0a28051207153f92c957cae7e457
Gabriel Conceicao <gabriel.bc.dev@hotmail.com>
2022-11-26 16:15:05 -0300

    Implementação da autenticação JWT parte 1

---
+++ CleanArchMvc.API/Models/RegisterModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace CleanArchMvc.API.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Password don´t match")]
+        public string ConfirmPassword { get; set;}
+    }
+}

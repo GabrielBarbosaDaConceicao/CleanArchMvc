30b9c04b86f4c18aa00d13fd61657884ef33d8ee
Gabriel Conceicao <gabriel.bc.dev@hotmail.com>
2022-11-26 08:10:09 -0300

    Add os endpoints Post / Put / Delete

--- CleanArchMvc.API/Controllers/CategoriesController.cs
+++ CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -29,7 +29,7 @@ namespace CleanArchMvc.API.Controllers
             return Ok(categories);
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "GetCategory" )]
         public async Task<ActionResult<CategoryDTO>> Get(int? id)
         {
             var category = await _categoryService.GetById(id);
@@ -40,5 +40,43 @@ namespace CleanArchMvc.API.Controllers
             return Ok(category);
         }
 
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CategoryDTO categoryDto)
+        {
+            if (categoryDto == null)
+                return BadRequest("Invalid Data");
+            
+            await _categoryService.Add(categoryDto);
+
+            return new CreatedAtRouteResult("GetCategory", new { id = categoryDto.Id },
+                categoryDto);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(int? id, [FromBody] CategoryDTO categoryDto)
+        {
+            if (id != categoryDto.Id)
+            return BadRequest();
+
+            if (categoryDto == null)
+             return BadRequest();
+
+            await _categoryService.Update(categoryDto);
+
+            return Ok(categoryDto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<CategoryDTO>> Delete(int? id)
+        {
+            var category = await _categoryService.GetById(id);
+
+            if (category == null)
+                return NotFound("Category not found");
+
+            await _categoryService.Remove(id);
+
+            return Ok(category);
+        }
     }
 }

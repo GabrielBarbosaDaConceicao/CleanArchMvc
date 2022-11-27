ea09fbb136013b1189d5e43779432c5feafbd38f
Gabriel Conceicao <gabriel.bc.dev@hotmail.com>
2022-11-27 09:29:34 -0300

    Add endpoint para criar novo usuario / CriateUser

--- CleanArchMvc.API/Controllers/TokenContoller.cs
+++ CleanArchMvc.API/Controllers/TokenContoller.cs
@@ -22,6 +22,24 @@ namespace CleanArchMvc.API.Controllers
             _configuration = configuration;
         }
 
+        [HttpPost("CreateUser")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public async Task<ActionResult> CreateUser([FromBody] LoginModel userInfo)
+        {
+            var result = await _authentication.RegisterUser(userInfo.Email, userInfo.Password);
+
+            if (result)
+            {
+                //return GenerateToken(userInfo);
+                return Ok($"User {userInfo.Email} was create successfully");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid Login attempt.");
+                return BadRequest(ModelState);
+            }
+        }
+
         [HttpPost("LoginUser")]
         public async Task<ActionResult<UserToken>> Login([FromBody] LoginModel userInfo)
         {
